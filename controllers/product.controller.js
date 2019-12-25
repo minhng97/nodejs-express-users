2f7e918dc0202f3e1c9ecf2471c170ae5aa69cfc
minhng97 <minh.nguyentrong97@gmail.com>
2019-12-25 13:41:44 +0700

    Changed to mongodb

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,27 +1,23 @@
 var Product = require('../models/product.model');
 
-//const db = require('../db');
-
 module.exports.index = async function(req, res) {
-// 	var allProducts = db.get('products').value().length;
 
-// 	var page = parseInt(req.query.page) || 1;
-// 	var perPage = 8;
-
-// 	var start = (page - 1) * perPage;
-// 	var end = page * perPage;
-
-// 	var drop = (page - 1) * perPage;
-// 	var lastPage = Math.ceil(allProducts / perPage); 
-
-// 	res.render('products/index', { 
-// 	products: db.get('products').drop(drop).take(perPage).value(),
-// 	page: page,
-// 	end: lastPage
-// }
-// )
 	var products = await Product.find();
+
+		var allProducts = await products.length;
+
+		var page = await parseInt(req.query.page) || 1;
+		var perPage = 8;
+
+		var start = await (page - 1) * perPage;
+		var end = await page * perPage;
+
+		var drop = await (page - 1) * perPage;
+		var lastPage = await Math.ceil(allProducts / perPage);
+
 		res.render('products/index', {
-			products: products
+			products: products.slice(drop, page*perPage),
+			page: page,
+			end: lastPage
 	})
 };
