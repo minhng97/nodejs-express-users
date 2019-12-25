85239863693971a084ab88e93862d1a820d9c3d1
minhng97 <minh.nguyentrong97@gmail.com>
2019-12-25 12:23:09 +0700

    Changed into mongodb

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,18 +1,20 @@
-var db = require('../db');
+var User = require('../models/user.model');
 
-module.exports.requireAuth = (req, res, next) => {
+module.exports.requireAuth = async (req, res, next) => {
 	if(!req.signedCookies.userId) { // if no cookie redirect to login page
 		res.redirect('/auth/login');
 		return;
+
 	}
+	var user = await User.find();
+	matchedUser = await user.filter(user => user._id === req.signedCookies.userId);
 
-	var user = db.get('users').find({ id: req.signedCookies.userId }).value();
-
-	if(!user) {	// if cookies.userId is wrong redirect to login page
+	if(!matchedUser[0]) {	// if cookies.userId is wrong redirect to login page
 		res.redirect('/auth/login');
 		return;
+
 	}
 
-	res.locals.user = user;
+	res.locals.user = matchedUser[0];
 	next(); // to next route
 }
