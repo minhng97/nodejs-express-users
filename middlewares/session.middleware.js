a51a16268ab8cf76a2c76d0287b6230ae14ba894
minhng97 <minh.nguyentrong97@gmail.com>
2019-12-26 23:04:23 +0700

    Edit: switched to mongodb

--- middlewares/session.middleware.js
+++ middlewares/session.middleware.js
@@ -8,9 +8,10 @@ module.exports = async function(req, res, next) {
 		signed: true
 	});
 
-	 var newSession = await new Session({
+	var newSession = await new Session({
 	 	id: sessionId, cart: {}
 	 });
+	
 	newSession.save(function (err, ses) {
       if (err) return console.error(err);
       console.log(ses.id + " saved to sessions collection.")});
