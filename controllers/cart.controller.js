e1ee9ad5599c92234138f9c2cd6996960e01792c
minhng97 <minh.nguyentrong97@gmail.com>
2019-12-27 18:13:54 +0700

    Modified: for easier view

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,4 +1,5 @@
 var Session = require('../models/session.model');
+var Product = require('../models/product.model');
 
 module.exports.addToCart = async function(req, res, next) {
 	var productId = req.params.productId;
@@ -9,17 +10,39 @@ module.exports.addToCart = async function(req, res, next) {
 		return;
 	}
 
-	var session = await Session.find();
-	var currentSession = session.filter(x => x.id === sessionId)[0];
-	var currentProduct =  currentSession.cart.get(productId);
+	var sessions = await Session.find();
+	var currentSession = sessions.find(ses => ses.id === sessionId);
+	var currentCart = currentSession.cart;
+	var currentProduct =  currentCart.get(productId);
 	
 	if (currentProduct) {
-		currentSession.cart.set(productId, currentProduct + 1);
+		currentCart.set(productId, currentProduct + 1);
 		currentSession.save();
 	}
-	else {currentSession.cart.set(productId, 1);
-	currentSession.save();}
+	else {currentCart.set(productId, 1);
+		currentSession.save();
+	}
 
 
 	res.redirect('/products');
+};
+
+module.exports.myCart = async function(req, res, next) {
+	var sessionId = req.signedCookies.sessionId;
+	var sessions = await Session.find();
+
+	var currentSession = sessions.find(ses => ses.id === sessionId);
+
+	var currentCart = currentSession.cart;
+
+	var dbProducts = await Product.find();
+
+var t = Object.entries(currentCart);
+var l = (t[0][1].cart)
+console.log((l))
+	//var renderProduct = dbProducts.map(product => product._id === currentCart )
+
+//	console.log(Object.entries(currentCart) );
+	res.render('../views/cartView', { products: currentCart })
+
 }
