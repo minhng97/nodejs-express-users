bb2d197f5ae6ea83952ff9c7d19daee2cf4bac5b
minhng97 <minh.nguyentrong97@gmail.com>
2020-01-04 18:56:05 +0700

    Add: ObjectId

--- models/session.model.js
+++ models/session.model.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
-
+var ObjectId = mongoose.Schema.Types.ObjectId;
 var sessionSchema = new mongoose.Schema({
+	_id: ObjectId,
 	id: String,
 	cart: 
 	{ type: Map, of: Number	}
