888b5addc406fd0ca33cafa8e9b95f7342c02ad4
minhng97 <minh.nguyentrong97@gmail.com>
2020-01-04 18:56:45 +0700

    Add: objectID + fix schema postionning

--- models/user.model.js
+++ models/user.model.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
-
+var ObjectId = mongoose.Schema.Types.ObjectId;
 var userSchema = new mongoose.Schema({
-	email: String,
-	password: String,
+	_id: ObjectId,
 	name: String,
-	avatar: String,
 	phone: String,
-	_id: String
+	password: String,
+	avatar: String,
+	email: String,
 });
 
-var User = mongoose.model('User', userSchema, 'users');
+var User = mongoose.model('User', userSchema, "users");
 
 module.exports = User;
